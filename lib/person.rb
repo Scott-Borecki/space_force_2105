a1b177a31f57e006fae85388d3e0244019a10a7c
Scott-Borecki <scottborecki@gmail.com>
2021-06-03 09:13:07 -0600

    Create readable attributes

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,13 @@
 class Person
 
+  attr_reader :name,
+              :experience,
+              :specialties
+
   def initialize(name, experience)
+    @name        = name
+    @experience  = experience
+    @specialties = []
   end
 
 end
