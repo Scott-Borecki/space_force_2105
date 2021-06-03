0308796e15996d05575ac5e39f0bff0849c5aceb
Scott-Borecki <scottborecki@gmail.com>
2021-06-03 09:29:03 -0600

    Create readable attributes: Pass

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -1,6 +1,13 @@
 class Flotilla
 
+  attr_reader :name,
+              :personnel,
+              :ships
+
   def initialize(data)
+    @name      = data[:designation]
+    @personnel = []
+    @ships     = []
   end
 
 end
