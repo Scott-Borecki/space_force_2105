e3b38bb5e9de8c74e8f91b31bcc14322c439c42c
Scott-Borecki <scottborecki@gmail.com>
2021-06-03 10:56:13 -0600

    Create #ready_ships & #requirements_staffed?: Pass

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -22,7 +22,8 @@ class Flotilla
     @personnel.find_all do |person|
       person.specialties.find do |specialty|
         spacecraft.requirements.find do |requirement|
-          requirement[specialty].is_a?(Integer) && requirement[specialty] <= person.experience
+          requirement[specialty].is_a?(Integer) &&
+            requirement[specialty] <= person.experience
         end
       end
     end
@@ -34,6 +35,20 @@ class Flotilla
       hash[ship] = recommend_personnel(ship)
     end
     hash
+  end
+
+  def requirements_staffed?(ship)
+    ship.requirements.all? do |requirement|
+      recommend_personnel(ship).any? do |person|
+        person.specialties.include?(requirement.keys[0]) && person.experience >= requirement.values[0]
+      end
+    end
+  end
+
+  def ready_ships(fuel)
+    @ships.find_all do |ship|
+      requirements_staffed?(ship) && ship.fuel >= fuel
+    end
   end
 
 end
