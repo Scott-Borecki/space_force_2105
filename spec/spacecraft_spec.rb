0cf0b09456e778f70fb3c699b6d8de60d23c0904
Scott-Borecki <scottborecki@gmail.com>
2021-06-03 09:07:55 -0600

    Test that Spacecraft has readable attributes

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -3,11 +3,19 @@ require './lib/person'
 
 RSpec.describe Spacecraft do
 
+  before :each do
+  @daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+  end
+
   describe 'Object Creation' do
 
     it 'exists' do
-      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
-      expect(daedalus).to be_a(Spacecraft)
+      expect(@daedalus).to be_a(Spacecraft)
+    end
+
+    it 'has readable attributes' do
+      expect(@daedalus.name).to eq('Daedalus')
+      expect(@daedalus.fuel).to eq(400)
     end
 
   end
